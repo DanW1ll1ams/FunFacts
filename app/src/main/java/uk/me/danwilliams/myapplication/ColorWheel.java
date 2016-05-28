93adabf27ee83f13db54f5998698694fb906f827
SimpleSols <danwilliams23.dw@gmail.com>
2016-05-28 18:44:25 +0100

    Initial commit

---
+++ app/src/main/java/uk/me/danwilliams/myapplication/ColorWheel.java
@@ -0,0 +1,36 @@
+package uk.me.danwilliams.myapplication;
+
+import android.graphics.Color;
+
+import java.util.Random;
+
+/**
+ * Created by Dan on 27/05/2016.
+ */
+public class ColorWheel {
+    //fields (member Variables)
+    private String[] mColors = {
+            "#39add1", // light blue
+            "#3079ab", // dark blue
+            "#c25975", // mauve
+            "#e15258", // red
+            "#f9845b", // orange
+            "#838cc7", // lavender
+            "#7d669e", // purple
+            "#53bbb4", // aqua
+            "#51b46d", // green
+            "#e0ab18", // mustard
+            "#637a91", // dark gray
+            "#f092b0", // pink
+            "#b7c0c7"  // light gray
+    };
+
+    //Methods
+    public int getColor(){
+        //Randomly select a color
+        Random randomGenerator = new Random();
+        int randomNumber = randomGenerator.nextInt(mColors.length);
+
+        return Color.parseColor(mColors[randomNumber]);
+    }
+}
